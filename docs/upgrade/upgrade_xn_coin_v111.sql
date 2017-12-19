d3d47622edd0da54912faee64408d2b5a6eb0f99
haiqingzheng <leo.zheng@hichengdai.com>
2017-12-19 17:29:05 +0800

    下架逻辑 脚本修改

--- docs/upgrade/upgrade_xn_coin_v111.sql
+++ docs/upgrade/upgrade_xn_coin_v111.sql
@@ -6,5 +6,13 @@ ADD COLUMN `div_rate2` DECIMAL(18,8) NULL COMMENT '分成比例2' AFTER `div_rat
 /*默认设置分成比例*/
 UPDATE `tstd_user` SET `div_rate1`='0.001', `div_rate2`='0.01' WHERE `kind`='C';
 
+/*合并待交易1和交易中2状态为上架状态1，已下架(3)状态变为2*/
+UPDATE `tcoin_ads` SET `status`='1' WHERE `status` in ('2');
+UPDATE `tcoin_ads` SET `status`='2' WHERE `status` in ('3');
+/*更新广告状态数据字典*/
+UPDATE `tsys_dict` SET `dvalue`='已上架' WHERE `parent_key`='ads_status' AND `dkey`='1';
+UPDATE `tsys_dict` SET `dvalue`='已下架' WHERE `parent_key`='ads_status' AND `dkey`='2';
+DELETE FROM `tsys_dict` WHERE `parent_key`='ads_status' AND `dkey`='3';
+
 
 
--- src/main/java/com/cdkj/coin/enums/EAdsStatus.java
+++ src/main/java/com/cdkj/coin/enums/EAdsStatus.java
@@ -8,7 +8,7 @@ public enum EAdsStatus {
     // DRAFT("0", "草稿"), DAIJIAOYI("1", "待交易"), JIAOYIZHONG("2", "交易中"), XIAJIA(
     // "3", "已下架");
 
-    DRAFT("0", "草稿"), SHANGJIA("1", "待交易"), XIAJIA("2", "已下架");
+    DRAFT("0", "草稿"), SHANGJIA("1", "已上架"), XIAJIA("2", "已下架");
 
     EAdsStatus(String code, String value) {
         this.code = code;
