55e375053620462a5c4b0c94557bd1c8bc50a413
haiqingzheng <leo.zheng@hichengdai.com>
2017-12-22 10:23:01 +0800

    udpate

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -41,7 +41,7 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.1.0 \n");
+        writer.append("Version:1.1.1 \n");
         writer.append("Description:eth 1st \n");
         writer.flush();
     }
