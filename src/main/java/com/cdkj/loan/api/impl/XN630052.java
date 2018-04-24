63119bc296a375959d642bd9609fee146ce642d9
nyc <yachao.ning@hichengdai.com>
2018-04-24 19:53:28 +0800

    删除公司编号

--- src/main/java/com/cdkj/loan/api/impl/XN630052.java
+++ src/main/java/com/cdkj/loan/api/impl/XN630052.java
@@ -5,7 +5,7 @@ import com.cdkj.loan.api.AProcessor;
 import com.cdkj.loan.common.JsonUtil;
 import com.cdkj.loan.core.ObjValidater;
 import com.cdkj.loan.dto.req.XN630052Req;
-import com.cdkj.loan.dto.res.PKCodeRes;
+import com.cdkj.loan.dto.res.XN630052Res;
 import com.cdkj.loan.exception.BizException;
 import com.cdkj.loan.exception.ParaException;
 import com.cdkj.loan.spring.SpringContextHolder;
@@ -24,7 +24,7 @@ public class XN630052 extends AProcessor {
 
     @Override
     public Object doBusiness() throws BizException {
-        return new PKCodeRes(
+        return new XN630052Res(
             userAO.doLogin(req.getLoginName(), req.getLoginPwd()));
     }
 
---
+++ src/main/java/com/cdkj/loan/dto/res/XN630052Res.java
@@ -0,0 +1,22 @@
+package com.cdkj.loan.dto.res;
+
+public class XN630052Res {
+
+    private String userId;
+
+    public XN630052Res() {
+    }
+
+    public XN630052Res(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+}
