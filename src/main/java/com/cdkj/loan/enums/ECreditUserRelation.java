3b92a878beeb2c6df9218674a6a53c563ff9e2fd
jiafr <329874982@qq.com>
2018-05-28 00:39:50 +0800

    修改 ECreditUserRelation

--- src/main/java/com/cdkj/loan/enums/ECreditUserRelation.java
+++ src/main/java/com/cdkj/loan/enums/ECreditUserRelation.java
@@ -2,7 +2,7 @@ package com.cdkj.loan.enums;
 
 public enum ECreditUserRelation {
 
-    SELF("1", ""), HUSBAND("2", "共还人"), WIFE("3", "担保人"), FATHER("4", "父亲"), MOTHER(
+    SELF("1", "贷款人本人"), HUSBAND("2", "共还人"), WIFE("3", "担保人"), FATHER("4", "父亲"), MOTHER(
             "5", "母亲"), FATHERINLAW("6", "岳父"), MOTHERINLAW("7", "岳母"), FRIEND(
             "8", "朋友");
 
