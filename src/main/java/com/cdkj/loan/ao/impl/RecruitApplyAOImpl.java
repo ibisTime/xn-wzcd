fabddb9af5d03a4eb8065e185f2c78a6f8b46a88
ITessence <yunlai0821@163.com>
2018-06-04 23:58:51 +0800

    人才申请

--- src/main/java/com/cdkj/loan/ao/impl/RecruitApplyAOImpl.java
+++ src/main/java/com/cdkj/loan/ao/impl/RecruitApplyAOImpl.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import com.cdkj.loan.ao.IRecruitApplyAO;
 import com.cdkj.loan.bo.IRecruitApplyBO;
 import com.cdkj.loan.bo.base.Paginable;
+import com.cdkj.loan.common.DateUtil;
+import com.cdkj.loan.core.StringValidater;
 import com.cdkj.loan.domain.RecruitApply;
+import com.cdkj.loan.dto.req.XN632480Req;
 import com.cdkj.loan.exception.BizException;
 
 @Service
@@ -18,7 +21,24 @@ public class RecruitApplyAOImpl implements IRecruitApplyAO {
     private IRecruitApplyBO recruitApplyBO;
 
     @Override
-    public String addRecruitApply(RecruitApply data) {
+    public String addRecruitApply(XN632480Req req) {
+        RecruitApply data = new RecruitApply();
+        data.setDepartmentCode(req.getDepartmentCode());
+        data.setPosition(req.getPosition());
+        data.setEstablishQuantity(
+            StringValidater.toInteger(req.getEstablishQuantity()));
+        data.setNowQuantity(StringValidater.toInteger(req.getNowQuantity()));
+        data.setApplyQuantity(
+            StringValidater.toInteger(req.getApplyQuantity()));
+        data.setApplyDatetime(DateUtil.strToDate(req.getApplyDatetime(),
+            DateUtil.FRONT_DATE_FORMAT_STRING));
+        data.setRequireDatetime(DateUtil.strToDate(req.getRequireDatetime(),
+            DateUtil.FRONT_DATE_FORMAT_STRING));
+        data.setReplaceRealName(req.getReplaceRealName());
+        data.setNewApplyReason(req.getNewApplyReason());
+        data.setPositionNowQuantity(
+            StringValidater.toInteger(req.getPositionNowQuantity()));
+        data.setPositionAddReason(req.getPositionAddReason());
         return recruitApplyBO.saveRecruitApply(data);
     }
 
