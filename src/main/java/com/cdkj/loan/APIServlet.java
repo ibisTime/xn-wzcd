4cb0fb495a02bb72b3ed66c6541e19c9ef4573f4
ITessence <yunlai0821@163.com>
2018-06-08 20:38:12 +0800

    apis

--- src/main/java/com/cdkj/loan/APIServlet.java
+++ src/main/java/com/cdkj/loan/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:1.0.0 \n");
-        writer.append("Description:htwt_laster_3rd \n");
+        writer.append("Description:htwt_laster_1234 \n");
         writer.flush();
     }
 
