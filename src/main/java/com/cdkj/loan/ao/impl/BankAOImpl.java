c80da62f1e8a053b7a87872669c2b45915eec533
ITessence <yunlai0821@163.com>
2018-06-16 14:22:43 +0800

    银行利率明细修改

--- src/main/java/com/cdkj/loan/ao/impl/BankAOImpl.java
+++ src/main/java/com/cdkj/loan/ao/impl/BankAOImpl.java
@@ -14,6 +14,8 @@ import com.cdkj.loan.bo.IBankSubbranchBO;
 import com.cdkj.loan.bo.base.Paginable;
 import com.cdkj.loan.core.StringValidater;
 import com.cdkj.loan.domain.Bank;
+import com.cdkj.loan.domain.BankRate;
+import com.cdkj.loan.domain.BankSubbranch;
 import com.cdkj.loan.dto.req.XN632030Req;
 import com.cdkj.loan.dto.req.XN632032Req;
 import com.cdkj.loan.enums.EBoolean;
@@ -59,7 +61,7 @@ public class BankAOImpl implements IBankAO {
         String code = bankBO.saveBank(data);
 
         // 保存利率明细
-        // bankRateBO.saveBankRate(req.getBankRateList(), code);
+        bankRateBO.saveBankRate(req.getBankRateList(), code);
         return code;
     }
 
@@ -69,23 +71,23 @@ public class BankAOImpl implements IBankAO {
         bankBO.dropBank(code);
 
         // 删除利率明细
-        // BankRate rateCondition = new BankRate();
-        // rateCondition.setBankCode(code);
-        // bankRateBO.dropBankRate(rateCondition);
+        BankRate rateCondition = new BankRate();
+        rateCondition.setBankCode(code);
+        bankRateBO.dropBankRate(rateCondition);
 
         // 删除支行信息
-        // BankSubbranch subCondition = new BankSubbranch();
-        // subCondition.setBankCode(code);
-        // bankSubbranchBO.dropBankSubbranch(subCondition);
+        BankSubbranch subCondition = new BankSubbranch();
+        subCondition.setBankCode(code);
+        bankSubbranchBO.dropBankSubbranch(subCondition);
     }
 
     @Override
     @Transactional
     public int editBank(XN632032Req req) {
-        // // 删除利率明细
-        // BankRate rateCondition = new BankRate();
-        // rateCondition.setBankCode(req.getCode());
-        // bankRateBO.dropBankRate(rateCondition);
+        // 删除利率明细
+        BankRate rateCondition = new BankRate();
+        rateCondition.setBankCode(req.getCode());
+        bankRateBO.dropBankRate(rateCondition);
 
         // 编辑银行信息
         Bank data = new Bank();
@@ -105,47 +107,47 @@ public class BankAOImpl implements IBankAO {
         data.setRemark(req.getRemark());
 
         // 保存利率明细
-        // bankRateBO.saveBankRate(req.getBankRateList(), req.getCode());
+        bankRateBO.saveBankRate(req.getBankRateList(), req.getCode());
         return bankBO.editBank(data);
     }
 
     @Override
     public Bank getBank(String code) {
         Bank data = bankBO.getBank(code);
-        //
-        // BankRate rateCondition = new BankRate();
-        // rateCondition.setBankCode(code);
-        // data.setBankRateList(bankRateBO.queryBankRateList(rateCondition));
+
+        BankRate rateCondition = new BankRate();
+        rateCondition.setBankCode(code);
+        data.setBankRateList(bankRateBO.queryBankRateList(rateCondition));
         return data;
     }
 
     @Override
     public Paginable<Bank> queryBankPage(int start, int limit, Bank condition) {
         Paginable<Bank> page = bankBO.getPaginable(start, limit, condition);
-        // List<Bank> bankList = page.getList();
-        // List<BankRate> bankRateList = null;
-        // BankRate rateCondition = new BankRate();
-        //
-        // // 添加利率明细信息
-        // for (Bank data : bankList) {
-        // rateCondition.setBankCode(condition.getCode());
-        // bankRateList = bankRateBO.queryBankRateList(rateCondition);
-        // data.setBankRateList(bankRateList);
-        // }
+        List<Bank> bankList = page.getList();
+        List<BankRate> bankRateList = null;
+        BankRate rateCondition = new BankRate();
+
+        // 添加利率明细信息
+        for (Bank data : bankList) {
+            rateCondition.setBankCode(condition.getCode());
+            bankRateList = bankRateBO.queryBankRateList(rateCondition);
+            data.setBankRateList(bankRateList);
+        }
         return page;
     }
 
     @Override
     public List<Bank> queryBankList(Bank condition) {
         List<Bank> bankList = bankBO.queryBankList(condition);
-        // BankRate rateCondition = new BankRate();
-        // List<BankRate> bankRateList = null;
-        // // 添加利率明细信息
-        // for (Bank data : bankList) {
-        // rateCondition.setBankCode(condition.getCode());
-        // bankRateList = bankRateBO.queryBankRateList(rateCondition);
-        // data.setBankRateList(bankRateList);
-        // }
+        BankRate rateCondition = new BankRate();
+        List<BankRate> bankRateList = null;
+        // 添加利率明细信息
+        for (Bank data : bankList) {
+            rateCondition.setBankCode(condition.getCode());
+            bankRateList = bankRateBO.queryBankRateList(rateCondition);
+            data.setBankRateList(bankRateList);
+        }
 
         return bankList;
     }
