5510cae9a1faf42547223c6308d8b45939a5e262
jiafr <329874982@qq.com>
2018-06-20 07:17:15 +0800

    xiugai

--- src/main/java/com/cdkj/loan/api/impl/XN632174.java
+++ src/main/java/com/cdkj/loan/api/impl/XN632174.java
@@ -5,7 +5,7 @@ import com.cdkj.loan.api.AProcessor;
 import com.cdkj.loan.common.JsonUtil;
 import com.cdkj.loan.core.ObjValidater;
 import com.cdkj.loan.dto.req.XN632174Req;
-import com.cdkj.loan.dto.res.PKCodeRes;
+import com.cdkj.loan.dto.res.BooleanRes;
 import com.cdkj.loan.exception.BizException;
 import com.cdkj.loan.exception.ParaException;
 import com.cdkj.loan.spring.SpringContextHolder;
@@ -24,8 +24,8 @@ public class XN632174 extends AProcessor {
 
     @Override
     public Object doBusiness() throws BizException {
-
-        return new PKCodeRes(totalAdvanceFundAO.addTotalAdvanceFund(req));
+        totalAdvanceFundAO.addTotalAdvanceFund(req);
+        return new BooleanRes(true);
     }
 
     @Override
