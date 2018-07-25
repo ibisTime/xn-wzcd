4175baee296235dacffc96d2e6db720056523b1d
ITessence <yunlai0821@163.com>
2018-07-25 19:59:32 +0800

    银行合同银行转义

--- docs/update/update_xn_wzcd103.sql
+++ docs/update/update_xn_wzcd103.sql
@@ -1,24 +1,10 @@
-UPDATE `tsys_node` SET `name`='总公司寄送银行材料' WHERE `code`='007_02';
-UPDATE `tsys_node` SET `name`='改成寄件岗寄件' WHERE `code`='007_05';
-
-
-DROP TABLE IF EXISTS `tdq_contract_import`;
-CREATE TABLE `tdq_contract_import` (
-  `code` varchar(32) NOT NULL COMMENT '编号',
-  `contract_code` varchar(32) NOT NULL COMMENT '合同号',
-  `budget_order_code` varchar(32) DEFAULT NULL COMMENT '预算单编号',
-  `customer_name` varchar(255) DEFAULT NULL COMMENT '客户姓名',
-  `id_no` varchar(32) DEFAULT NULL COMMENT '身份证号',
-  `loan_amount` bigint(20) DEFAULT NULL COMMENT '贷款金额',
-  `bank_code` varchar(32) DEFAULT NULL COMMENT '银行',
-  `bill_datetime` int(11) DEFAULT NULL COMMENT '账单日',
-  `repay_bank_date` int(11) DEFAULT NULL COMMENT '还款日',
-  `bank_card_number` varchar(32) DEFAULT NULL COMMENT '信用卡号',
-  `contract_sign_date` datetime DEFAULT NULL COMMENT '合同签订日',
-  `import_datetime` datetime DEFAULT NULL COMMENT '导入日期',
-  `status` varchar(4) DEFAULT NULL COMMENT '状态',
-  `operator` varchar(32) DEFAULT NULL COMMENT '操作人',
-  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`code`) COMMENT '银行合同导入'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='银行合同导入';
-
+UPDATE `tsys_node` SET `name`='风控总监理审核' WHERE `code`='020_03';
+UPDATE `tsys_node` SET `name`='财务审核' WHERE `code`='020_04';
+UPDATE `tsys_node` SET `name`='确认付款' WHERE `code`='020_05';
+UPDATE `tsys_node` SET `name`='风控经理审核' WHERE `code`='020_08';
+UPDATE `tsys_node` SET `name`='打印岗打印' WHERE `code`='020_09';
+UPDATE `tsys_node` SET `name`='寄送银行材料' WHERE `code`='020_10';
+UPDATE `tsys_node` SET `name`='驻行人员提交材料' WHERE `code`='020_11';
+UPDATE `tsys_node` SET `name`='抵押完成' WHERE `code`='020_12';
+DELETE FROM `tsys_node` WHERE `code`='020_13';
+DELETE FROM `tsys_node` WHERE `code`='020_14';
