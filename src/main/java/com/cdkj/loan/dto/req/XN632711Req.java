2339768185ac770454387f76d2a0490c6ce9db3d
jiafr <329874982@qq.com>
2018-08-12 14:14:35 +0800

    gps

--- src/main/java/com/cdkj/loan/dto/req/XN632711Req.java
+++ src/main/java/com/cdkj/loan/dto/req/XN632711Req.java
@@ -1,9 +1,6 @@
 package com.cdkj.loan.dto.req;
 
-import java.util.List;
-
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * GPS个人申领
@@ -16,9 +13,9 @@ public class XN632711Req {
     @NotBlank
     private String applyUser;
 
-    // GPS列表
-    @NotEmpty
-    private List<XN632712ReqGps> gpsList;
+    // 申请数量
+    @NotBlank
+    private String applyCount;
 
     // 申请原因
     private String applyReason;
@@ -31,12 +28,12 @@ public class XN632711Req {
         this.applyUser = applyUser;
     }
 
-    public List<XN632712ReqGps> getGpsList() {
-        return gpsList;
+    public String getApplyCount() {
+        return applyCount;
     }
 
-    public void setGpsList(List<XN632712ReqGps> gpsList) {
-        this.gpsList = gpsList;
+    public void setApplyCount(String applyCount) {
+        this.applyCount = applyCount;
     }
 
     public String getApplyReason() {
