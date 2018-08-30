05a296cf2f92f4ab85216b38e84275ff1ab66877
jiafr <329874982@qq.com>
2018-08-30 17:02:04 +0800

    不报表笔数

--- src/main/java/com/cdkj/loan/dto/res/XN630909Res.java
+++ src/main/java/com/cdkj/loan/dto/res/XN630909Res.java
@@ -14,65 +14,129 @@ public class XN630909Res {
 
     private String oneGrowthRate;// 1月增长率
 
+    private String oneNum;// 1月笔数
+
+    private String oneNumGrowthRate;// 1月笔数增长率
+
     private String twoLoanAmount;// 2月贷款额
 
     private String twoGrowthRate;// 2月增长率
+
+    private String twoNum;// 2月笔数
+
+    private String twoNumGrowthRate;// 2月笔数增长率
 
     private String threeLoanAmount;// 3月贷款额
 
     private String threeGrowthRate;// 3月增长率
 
+    private String threeNum;// 3月笔数
+
+    private String threeNumGrowthRate;// 3月笔数增长率
+
     private String fourLoanAmount;// 4月贷款额
 
     private String fourGrowthRate;// 4月增长率
+
+    private String fourNum;// 4月笔数
+
+    private String fourNumGrowthRate;// 4月笔数增长率
 
     private String fiveLoanAmount;// 5月贷款额
 
     private String fiveGrowthRate;// 5月增长率
 
+    private String fiveNum;// 5月笔数
+
+    private String fiveNumGrowthRate;// 5月笔数增长率
+
     private String sixLoanAmount;// 6月贷款额
 
     private String sixGrowthRate;// 6月增长率
+
+    private String sixNum;// 6月笔数
+
+    private String sixNumGrowthRate;// 6月笔数增长率
 
     private String sevenLoanAmount;// 7月贷款额
 
     private String sevenGrowthRate;// 7月增长率
 
+    private String sevenNum;// 7月笔数
+
+    private String sevenNumGrowthRate;// 7月笔数增长率
+
     private String eightLoanAmount;// 8月贷款额
 
     private String eightGrowthRate;// 8月增长率
+
+    private String eightNum;// 8月笔数
+
+    private String eightNumGrowthRate;// 8月笔数增长率
 
     private String nineLoanAmount;// 9月贷款额
 
     private String nineGrowthRate;// 9月增长率
 
+    private String nineNum;// 9月笔数
+
+    private String nineNumGrowthRate;// 9月笔数增长率
+
     private String tenLoanAmount;// 10月贷款额
 
     private String tenGrowthRate;// 10月增长率
+
+    private String tenNum;// 10月笔数
+
+    private String tenNumGrowthRate;// 10月笔数增长率
 
     private String elevenLoanAmount;// 11月贷款额
 
     private String elevenGrowthRate;// 11月增长率
 
+    private String elevenNum;// 11月笔数
+
+    private String elevenNumGrowthRate;// 11月笔数增长率
+
     private String twelveLoanAmount;// 12月贷款额
 
     private String twelveGrowthRate;// 12月增长率
+
+    private String twelveNum;// 12月笔数
+
+    private String twelveNumGrowthRate;// 12月笔数增长率
 
     private String firstQuarterLoanAmount;// 一季度
 
     private String firstQuarterGrowthRate;// 一季度增长率
 
+    private String firstQuarterNum;// 一季度笔数
+
+    private String firstQuarterNumGrowthRate;// 一季度笔数增长率
+
     private String secondQuarterLoanAmount;// 二季度
 
     private String secondQuarterGrowthRate;// 二季度增长率
+
+    private String secondQuarterNum;// 二季度笔数
+
+    private String secondQuarterNumGrowthRate;// 二季度笔数增长率
 
     private String thirdQuarterLoanAmount;// 三季度
 
     private String thirdQuarterGrowthRate;// 三季度增长率
 
+    private String thirdQuarterNum;// 三季度笔数
+
+    private String thirdQuarterNumGrowthRate;// 三季度笔数增长率
+
     private String fourthQuarterLoanAmount;// 四季度
 
     private String fourthQuarterGrowthRate;// 四季度增长率
+
+    private String fourthQuarterNum;// 四季度笔数
+
+    private String fourthQuarterNumGrowthRate;// 四季度笔数增长率
 
     private String totalLoanAmount;// 全年合计贷款额
 
@@ -378,32 +442,308 @@ public class XN630909Res {
         this.totalNumberGrowthRate = totalNumberGrowthRate;
     }
 
-    public XN630909Res(String oneLoanAmount, String twoLoanAmount,
-            String threeLoanAmount, String fourLoanAmount,
-            String fiveLoanAmount, String sixLoanAmount,
-            String sevenLoanAmount, String eightLoanAmount,
-            String nineLoanAmount, String tenLoanAmount,
-            String elevenLoanAmount, String twelveLoanAmount,
-            String firstQuarterLoanAmount, String secondQuarterLoanAmount,
-            String thirdQuarterLoanAmount, String fourthQuarterLoanAmount,
-            String totalLoanAmount, String totalNumber) {
+    public String getOneNum() {
+        return oneNum;
+    }
+
+    public void setOneNum(String oneNum) {
+        this.oneNum = oneNum;
+    }
+
+    public String getOneNumGrowthRate() {
+        return oneNumGrowthRate;
+    }
+
+    public void setOneNumGrowthRate(String oneNumGrowthRate) {
+        this.oneNumGrowthRate = oneNumGrowthRate;
+    }
+
+    public String getTwoNum() {
+        return twoNum;
+    }
+
+    public void setTwoNum(String twoNum) {
+        this.twoNum = twoNum;
+    }
+
+    public String getTwoNumGrowthRate() {
+        return twoNumGrowthRate;
+    }
+
+    public void setTwoNumGrowthRate(String twoNumGrowthRate) {
+        this.twoNumGrowthRate = twoNumGrowthRate;
+    }
+
+    public String getThreeNum() {
+        return threeNum;
+    }
+
+    public void setThreeNum(String threeNum) {
+        this.threeNum = threeNum;
+    }
+
+    public String getThreeNumGrowthRate() {
+        return threeNumGrowthRate;
+    }
+
+    public void setThreeNumGrowthRate(String threeNumGrowthRate) {
+        this.threeNumGrowthRate = threeNumGrowthRate;
+    }
+
+    public String getFourNum() {
+        return fourNum;
+    }
+
+    public void setFourNum(String fourNum) {
+        this.fourNum = fourNum;
+    }
+
+    public String getFourNumGrowthRate() {
+        return fourNumGrowthRate;
+    }
+
+    public void setFourNumGrowthRate(String fourNumGrowthRate) {
+        this.fourNumGrowthRate = fourNumGrowthRate;
+    }
+
+    public String getFiveNum() {
+        return fiveNum;
+    }
+
+    public void setFiveNum(String fiveNum) {
+        this.fiveNum = fiveNum;
+    }
+
+    public String getFiveNumGrowthRate() {
+        return fiveNumGrowthRate;
+    }
+
+    public void setFiveNumGrowthRate(String fiveNumGrowthRate) {
+        this.fiveNumGrowthRate = fiveNumGrowthRate;
+    }
+
+    public String getSixNum() {
+        return sixNum;
+    }
+
+    public void setSixNum(String sixNum) {
+        this.sixNum = sixNum;
+    }
+
+    public String getSixNumGrowthRate() {
+        return sixNumGrowthRate;
+    }
+
+    public void setSixNumGrowthRate(String sixNumGrowthRate) {
+        this.sixNumGrowthRate = sixNumGrowthRate;
+    }
+
+    public String getSevenNum() {
+        return sevenNum;
+    }
+
+    public void setSevenNum(String sevenNum) {
+        this.sevenNum = sevenNum;
+    }
+
+    public String getSevenNumGrowthRate() {
+        return sevenNumGrowthRate;
+    }
+
+    public void setSevenNumGrowthRate(String sevenNumGrowthRate) {
+        this.sevenNumGrowthRate = sevenNumGrowthRate;
+    }
+
+    public String getEightNum() {
+        return eightNum;
+    }
+
+    public void setEightNum(String eightNum) {
+        this.eightNum = eightNum;
+    }
+
+    public String getEightNumGrowthRate() {
+        return eightNumGrowthRate;
+    }
+
+    public void setEightNumGrowthRate(String eightNumGrowthRate) {
+        this.eightNumGrowthRate = eightNumGrowthRate;
+    }
+
+    public String getNineNum() {
+        return nineNum;
+    }
+
+    public void setNineNum(String nineNum) {
+        this.nineNum = nineNum;
+    }
+
+    public String getNineNumGrowthRate() {
+        return nineNumGrowthRate;
+    }
+
+    public void setNineNumGrowthRate(String nineNumGrowthRate) {
+        this.nineNumGrowthRate = nineNumGrowthRate;
+    }
+
+    public String getTenNum() {
+        return tenNum;
+    }
+
+    public void setTenNum(String tenNum) {
+        this.tenNum = tenNum;
+    }
+
+    public String getTenNumGrowthRate() {
+        return tenNumGrowthRate;
+    }
+
+    public void setTenNumGrowthRate(String tenNumGrowthRate) {
+        this.tenNumGrowthRate = tenNumGrowthRate;
+    }
+
+    public String getElevenNum() {
+        return elevenNum;
+    }
+
+    public void setElevenNum(String elevenNum) {
+        this.elevenNum = elevenNum;
+    }
+
+    public String getElevenNumGrowthRate() {
+        return elevenNumGrowthRate;
+    }
+
+    public void setElevenNumGrowthRate(String elevenNumGrowthRate) {
+        this.elevenNumGrowthRate = elevenNumGrowthRate;
+    }
+
+    public String getTwelveNum() {
+        return twelveNum;
+    }
+
+    public void setTwelveNum(String twelveNum) {
+        this.twelveNum = twelveNum;
+    }
+
+    public String getTwelveNumGrowthRate() {
+        return twelveNumGrowthRate;
+    }
+
+    public void setTwelveNumGrowthRate(String twelveNumGrowthRate) {
+        this.twelveNumGrowthRate = twelveNumGrowthRate;
+    }
+
+    public String getFirstQuarterNum() {
+        return firstQuarterNum;
+    }
+
+    public void setFirstQuarterNum(String firstQuarterNum) {
+        this.firstQuarterNum = firstQuarterNum;
+    }
+
+    public String getFirstQuarterNumGrowthRate() {
+        return firstQuarterNumGrowthRate;
+    }
+
+    public void setFirstQuarterNumGrowthRate(String firstQuarterNumGrowthRate) {
+        this.firstQuarterNumGrowthRate = firstQuarterNumGrowthRate;
+    }
+
+    public String getSecondQuarterNum() {
+        return secondQuarterNum;
+    }
+
+    public void setSecondQuarterNum(String secondQuarterNum) {
+        this.secondQuarterNum = secondQuarterNum;
+    }
+
+    public String getSecondQuarterNumGrowthRate() {
+        return secondQuarterNumGrowthRate;
+    }
+
+    public void setSecondQuarterNumGrowthRate(String secondQuarterNumGrowthRate) {
+        this.secondQuarterNumGrowthRate = secondQuarterNumGrowthRate;
+    }
+
+    public String getThirdQuarterNum() {
+        return thirdQuarterNum;
+    }
+
+    public void setThirdQuarterNum(String thirdQuarterNum) {
+        this.thirdQuarterNum = thirdQuarterNum;
+    }
+
+    public String getThirdQuarterNumGrowthRate() {
+        return thirdQuarterNumGrowthRate;
+    }
+
+    public void setThirdQuarterNumGrowthRate(String thirdQuarterNumGrowthRate) {
+        this.thirdQuarterNumGrowthRate = thirdQuarterNumGrowthRate;
+    }
+
+    public String getFourthQuarterNum() {
+        return fourthQuarterNum;
+    }
+
+    public void setFourthQuarterNum(String fourthQuarterNum) {
+        this.fourthQuarterNum = fourthQuarterNum;
+    }
+
+    public String getFourthQuarterNumGrowthRate() {
+        return fourthQuarterNumGrowthRate;
+    }
+
+    public void setFourthQuarterNumGrowthRate(String fourthQuarterNumGrowthRate) {
+        this.fourthQuarterNumGrowthRate = fourthQuarterNumGrowthRate;
+    }
+
+    public XN630909Res(String oneLoanAmount, String oneNum,
+            String twoLoanAmount, String twoNum, String threeLoanAmount,
+            String threeNum, String fourLoanAmount, String fourNum,
+            String fiveLoanAmount, String fiveNum, String sixLoanAmount,
+            String sixNum, String sevenLoanAmount, String sevenNum,
+            String eightLoanAmount, String eightNum, String nineLoanAmount,
+            String nineNum, String tenLoanAmount, String tenNum,
+            String elevenLoanAmount, String elevenNum, String twelveLoanAmount,
+            String twelveNum, String firstQuarterLoanAmount,
+            String firstQuarterNum, String secondQuarterLoanAmount,
+            String secondQuarterNum, String thirdQuarterLoanAmount,
+            String thirdQuarterNum, String fourthQuarterLoanAmount,
+            String fourthQuarterNum, String totalLoanAmount, String totalNumber) {
         super();
         this.oneLoanAmount = oneLoanAmount;
+        this.oneNum = oneNum;
         this.twoLoanAmount = twoLoanAmount;
+        this.twoNum = twoNum;
         this.threeLoanAmount = threeLoanAmount;
+        this.threeNum = threeNum;
         this.fourLoanAmount = fourLoanAmount;
+        this.fourNum = fourNum;
         this.fiveLoanAmount = fiveLoanAmount;
+        this.fiveNum = fiveNum;
         this.sixLoanAmount = sixLoanAmount;
+        this.sixNum = sixNum;
         this.sevenLoanAmount = sevenLoanAmount;
+        this.sevenNum = sevenNum;
         this.eightLoanAmount = eightLoanAmount;
+        this.eightNum = eightNum;
         this.nineLoanAmount = nineLoanAmount;
+        this.nineNum = nineNum;
         this.tenLoanAmount = tenLoanAmount;
+        this.tenNum = tenNum;
         this.elevenLoanAmount = elevenLoanAmount;
+        this.elevenNum = elevenNum;
         this.twelveLoanAmount = twelveLoanAmount;
+        this.twelveNum = twelveNum;
         this.firstQuarterLoanAmount = firstQuarterLoanAmount;
+        this.firstQuarterNum = firstQuarterNum;
         this.secondQuarterLoanAmount = secondQuarterLoanAmount;
+        this.secondQuarterNum = secondQuarterNum;
         this.thirdQuarterLoanAmount = thirdQuarterLoanAmount;
+        this.thirdQuarterNum = thirdQuarterNum;
         this.fourthQuarterLoanAmount = fourthQuarterLoanAmount;
+        this.fourthQuarterNum = fourthQuarterNum;
         this.totalLoanAmount = totalLoanAmount;
         this.totalNumber = totalNumber;
     }
