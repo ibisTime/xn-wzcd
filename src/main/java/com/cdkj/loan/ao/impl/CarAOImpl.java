47cb1416581ac4c2fa11b9773ca4d57a9a1cccb1
ITessence <yunlai0821@163.com>
2018-09-06 00:41:51 +0800

    update

--- src/main/java/com/cdkj/loan/ao/impl/CarAOImpl.java
+++ src/main/java/com/cdkj/loan/ao/impl/CarAOImpl.java
@@ -1,17 +1,27 @@
 package com.cdkj.loan.ao.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.cdkj.loan.aliyun.util.HttpUtils;
 import com.cdkj.loan.ao.ICarAO;
 import com.cdkj.loan.bo.ICarBO;
 import com.cdkj.loan.bo.ISYSUserBO;
+import com.cdkj.loan.bo.ISeriesBO;
 import com.cdkj.loan.bo.base.Paginable;
 import com.cdkj.loan.core.StringValidater;
 import com.cdkj.loan.domain.Car;
+import com.cdkj.loan.domain.Series;
 import com.cdkj.loan.dto.req.XN630420Req;
 import com.cdkj.loan.dto.req.XN630422Req;
 import com.cdkj.loan.enums.EBrandStatus;
@@ -22,6 +32,9 @@ public class CarAOImpl implements ICarAO {
 
     @Autowired
     private ICarBO carBO;
+
+    @Autowired
+    private ISeriesBO seriesBO;
 
     @Autowired
     private ISYSUserBO sysUserBO;
@@ -50,6 +63,43 @@ public class CarAOImpl implements ICarAO {
 
         car.setRemark(req.getRemark());
         return carBO.saveCar(car);
+    }
+
+    private ArrayList<JSONArray> generateSeries() {
+        ArrayList<JSONArray> list = new ArrayList<JSONArray>();
+        JSONArray json = null;
+        String host = "https://jisucxdq.market.alicloudapi.com";
+        String path = "/car/carlist";
+        String method = "GET";
+        String appcode = "1bd9832a74284772a7549ff0cc51043e";
+        Map<String, String> headers = new HashMap<String, String>();
+        // 最后在header中的格式(中间是英文空格)为Authorization:APPCODE
+        // 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        ArrayList<String> arrayList = new ArrayList<String>();
+        Series condition = new Series();
+        List<Series> querySeries = seriesBO.querySeries(condition);
+        for (Series series : querySeries) {
+            arrayList.add(series.getRemark());
+        }
+        for (String seriesCode : arrayList) {
+            querys.put("parentid", seriesCode);
+            try {
+                HttpResponse response = HttpUtils.doGet(host, path, method,
+                    headers, querys);
+                HttpEntity entity = response.getEntity();
+                String string = EntityUtils.toString(entity);// 获取response的body
+                // System.out.println("1------------->" + string);
+                json = (JSONArray) JSONArray
+                    .parse(string.substring(34, string.length() - 1));
+                // System.out.println("2------------->" + json);
+                list.add(json);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 
     @Override
